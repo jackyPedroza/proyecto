3d789924c7c3b182a665574a37c863953f3c4896
jackyPedroza <jacki1744@hotmail.com>
2018-12-06 02:47:58 -0600

    Madrugada del jueves

--- app/src/main/java/com/example/jacky/nochisbands/ListaBandas.java
+++ app/src/main/java/com/example/jacky/nochisbands/ListaBandas.java
@@ -1,13 +1,27 @@
 package com.example.jacky.nochisbands;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ListaBandas extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_bandas);
-    }
+
 }
